132364b6d34bceebb58ea140d8778ed10bc0737c
SarthakU <sarthak7u@gmail.com>
2014-10-22 13:46:28 +0530

    Fixed bug
    Now encrypts an alphabet character to an alphabet character

--- Python/daily3(easy).py
+++ Python/daily3(easy).py
@@ -1,8 +1,19 @@
+
+## CAESAR CIPHER
+##
+## challenge #4 (easy)
+## http://www.reddit.com/r/dailyprogrammer/comments/pm6oj/2122012_challenge_4_easy/
+## 
+## 
+## sarthak7u@gmail.com
+##
+
 #encrypts 
 def encrypt(text, key):
 
 	cipherText = ""
 
+	i = 0
 	for letter in text:
 
 		#skips over non-alpha characters
@@ -10,7 +21,14 @@ def encrypt(text, key):
 			cipherText += letter
 		#ciphers using caesar cipher 
 		else:
-			cipherText += chr(ord(letter) + key)
+			if letter.isupper() == True:
+				tempLetter = chr((ord(letter) - ord("A") + key)%26 + ord("A"))
+			elif letter.islower() == True:
+				tempLetter = chr((ord(letter) - ord("a") + key)%26 + ord("a"))
+			cipherText += tempLetter
+
+			
+		i += 1
 
 	return cipherText
 
@@ -18,4 +36,7 @@ def encrypt(text, key):
 inputText = str(raw_input("Enter some text for me to Encrypt: "))
 key = int(raw_input("Enter a key to Encrypt: "))
 
+
 print encrypt(inputText, key)
+
+raw_input()
