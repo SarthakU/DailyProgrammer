6de510138f4ee4af3f16e5332ec61021de409000
Sarthak Upadhyay <sarthak7u@gmail.com>
2014-11-11 19:37:38 +0530

    Added option for decryption (C #003 easy (caesar))

--- C/Daily003_easy/Caesar.c
+++ C/Daily003_easy/Caesar.c
@@ -14,10 +14,17 @@
 #define MAX_LEN 30
 
 char* encrypt(char* inputText, int key);
+char* decrypt(char* inputText, int key);
 
 int main(void)
 {
 	
+	int choice;
+	printf("Enter 1 for encryption, 0 for decryption: ");
+	scanf("%d", &choice);
+	getchar();
+
+
 	//reads input text to be encrypted
 	char inputText[MAX_LEN] = {'\0'}; 
 	printf("\nEnter some text: ");
@@ -25,11 +32,23 @@ int main(void)
 
 	//reads key for encryption
 	int key;
-	printf("\nEnter a key to Encrypt: ");
+	printf("\nEnter a key: ");
 	scanf("%d", &key);
+	getchar();
 	char* outputText;
-	outputText = encrypt(inputText,key);
 		
+	switch(choice)
+	{
+		case 1:
+			outputText = encrypt(inputText,key);
+			break;
+		case 0:
+			outputText = decrypt(inputText,key);
+			break;
+		default:
+			outputText = encrypt(inputText,key);
+			break;
+	}
 	//calls encrypt function and prints out results
 	printf("%s\n", outputText);
 	
@@ -68,4 +87,50 @@ char* encrypt(char* inputText, int key)
 	}
 	return cipherText;
 
+}
+char* decrypt(char* inputText, int key)
+{
+	//allocates memory for variable 
+	char* text = malloc(sizeof(inputText));
+
+	//copies contents of inputText to cipherText
+	strcpy(text, inputText);
+
+	//temporary variables
+	int index = 0;
+
+	//iterates through each char in ciphertext until null character read
+	while (text[index] != '\0')
+	{
+		char currentChar = text[index];
+		if (isalpha(currentChar) == 0)
+		{}
+		else
+		{
+			if (isupper(currentChar) == 0)
+			{
+				if ((int) (currentChar - key - 'a') < 0)
+				{
+					text[index] =  text[index]  - key + 26;			
+				}
+				else
+				{
+					text[index] =  text[index]  - key;	
+				}
+			}
+			else
+			{	
+				if ((int) (currentChar - key - 'A') < 0)
+				{
+					text[index] =  text[index]  - key + 26;			
+				}
+				else
+				{
+					text[index] =  text[index]  - key;	
+				}
+			}	
+		}
+		index++;
+	}
+	return text;
 }
