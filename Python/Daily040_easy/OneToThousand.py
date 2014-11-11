fd1233e10fe0cc7d1fce37796f2eb4f8039b0052
Sarthak Upadhyay <sarthak7u@gmail.com>
2014-11-11 13:45:48 +0530

    Added challenge #40 (easy) in Python

---
+++ Python/Daily040_easy/OneToThousand.py
@@ -0,0 +1,9 @@
+## ONE TO THOUSAND
+##
+## challenge #40 (easy)
+## http://www.reddit.com/r/dailyprogrammer/comments/schtf/4162012_challenge_40_easy/
+## 
+## 
+## sarthak7u@gmail.com
+
+print " \n".join(map(str,range(1,101)))
