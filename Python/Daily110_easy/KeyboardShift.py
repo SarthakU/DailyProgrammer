754113f9c4979aef6c0bbb48d2dac1876dcb2fd7
Sarthak Upadhyay <sarthak7u@gmail.com>
2014-11-11 20:34:25 +0530

    Added challenge #110 (easy) in Python

---
+++ Python/Daily110_easy/KeyboardShift.py
@@ -0,0 +1,15 @@
+## KEYBOARD SHIFT
+##
+## challenge #110 (easy)
+## http://www.reddit.com/r/dailyprogrammer/comments/12k3xr/1132012_challenge_110_easy_keyboard_shift/
+## 
+## 
+## sarthak7u@gmail.com
+##
+
+a = str(raw_input("Enter a string: ")).lower(); ans = ""
+wrong = list("wertyuiop{[sdfghjkl;:xcvbnm,< ")
+right = list("qwertyuioppasdfghjkllzxcvbnmm ")
+for i in xrange(len(a)):
+	ans += right[wrong.index(a[i])]
+print ans
