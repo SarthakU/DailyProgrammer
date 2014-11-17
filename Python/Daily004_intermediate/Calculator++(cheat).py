b0439bf2c265c7df8b4b321c6ff2e9892d1390af
Sarthak Upadhyay <sarthak7u@gmail.com>
2014-11-17 19:44:58 +0530

    Added challenge #4 (intermediate) (cheat) in Python

---
+++ Python/Daily004_intermediate/Calculator++(cheat).py
@@ -0,0 +1,9 @@
+## CALCULATOR ++
+##
+## challenge #4 (intermediate)
+## http://www.reddit.com/r/dailyprogrammer/comments/pm6sq/2122012_challenge_4_intermediate/
+## 
+## 
+## sarthak7u@gmail.com
+##
+exp = input("Enter an expression : ");print exp
