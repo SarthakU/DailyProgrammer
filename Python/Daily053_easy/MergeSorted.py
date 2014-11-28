a5574e4883f5e0167619e5d5d3bbdc9e221d2792
Sarthak Upadhyay <sarthak7u@gmail.com>
2014-11-28 23:58:08 +0530

    Added challenge #53 (easy) in Python

---
+++ Python/Daily053_easy/MergeSorted.py
@@ -0,0 +1,10 @@
+## MERGE SORTED
+##
+## challenge #53 (easy)
+## http://www.reddit.com/r/dailyprogrammer/comments/tpxq9/5162012_challenge_53_easy/
+## 
+## 
+## sarthak7u@gmail.com
+##
+def merge_sorted(list1, list2):
+	return [i for i in sorted(list1+list2)]
