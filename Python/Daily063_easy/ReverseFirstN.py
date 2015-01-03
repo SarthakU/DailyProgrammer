04900a5c48f65b79a95da1a25bf8b7b1618981ec
Sarthak Upadhyay <sarthak7u@gmail.com>
2015-01-03 22:46:41 +0530

    Added challenge #63(easy) in Python

---
+++ Python/Daily063_easy/ReverseFirstN.py
@@ -0,0 +1,10 @@
+## REVERSE FIRST N
+##
+## challenge #063 (easy)
+## http://www.reddit.com/r/dailyprogrammer/comments/uw14f/6112012_challenge_63_easy/
+## 
+## 
+## sarthak7u@gmail.com
+##
+def reverse(N,A):
+	return A[N-1::-1] + A[N:]
