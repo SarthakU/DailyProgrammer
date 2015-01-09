43cab7c2846ab01f079ea8e645ecc7c21efa01de
Sarthak Upadhyay <sarthak7u@gmail.com>
2015-01-10 01:08:49 +0530

    bug fix (a = 1 now)

--- Ruby/Daily052_easy/SumTheWord.rb
+++ Ruby/Daily052_easy/SumTheWord.rb
@@ -1,12 +1,19 @@
+## SQUARE ROOT
+##
+## challenge #052 (easy)
+## http://www.reddit.com/r/dailyprogrammer/comments/tmnfq/5142012_challenge_52_easy/
+## 
+## 
+## sarthak7u@gmail.com
+##
 def sum_the_word(word)
-	alpha_weight = {} # hash to represent weight of each alphabet i.e. a is 0, b is 1 etc.
+	alpha_weight = {} # hash to represent weight of each alphabet i.e. a is 1, b is 2 etc.
 	('a'..'z').to_a.each do |i|
-		alpha_weight[i] = alpha_weight.length
+		alpha_weight[i] = alpha_weight.length + 1
 	end
 	sum = 0
-	word.each do |i|
-		sum += alpha_weight[i]
+	word.split("").each do |i|
+		sum += alpha_weight[i].to_i
 	end
 	return sum
 end
-sum_the_word(1)
