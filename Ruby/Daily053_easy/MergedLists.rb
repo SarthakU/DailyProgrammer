bc4ce6dcc9f6c31ca771c3562c89e719a4d4ca20
Sarthak Upadhyay <sarthak7u@gmail.com>
2015-01-10 21:37:20 +0530

    Added challenge #53(easy) in Ruby

---
+++ Ruby/Daily053_easy/MergedLists.rb
@@ -0,0 +1,11 @@
+## MERGED LISTS
+##
+## challenge #053 (easy)
+## http://www.reddit.com/r/dailyprogrammer/comments/tpxq9/5162012_challenge_53_easy/
+## 
+## 
+## sarthak7u@gmail.com
+##
+def merge_lists(listA, listB)
+	return (listA + listB).sort
+end
