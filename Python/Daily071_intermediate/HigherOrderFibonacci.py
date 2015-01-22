a41bb8a99f6b2c24082c2cad28b624e9002ed6d4
Sarthak Upadhyay <sarthak7u@gmail.com>
2015-01-22 23:25:25 +0530

    Add challenge #71(intermediate) in Python

---
+++ Python/Daily071_intermediate/HigherOrderFibonacci.py
@@ -0,0 +1,33 @@
+## HIGHER ORDER FIBONACCI
+##
+## challenge #71 (intermediate)
+## http://www.reddit.com/r/dailyprogrammer/comments/vx3db/722012_challenge_71_intermediate/
+## 
+## 
+## sarthak7u@gmail.com
+##
+def higher_k_fibonacci(k, N):
+	'''
+	Finds the N the term of Fibonacci series
+	order k
+
+	Args:
+		k: order of desired Fibonacci series
+		N: index of term in series need
+
+	Returns:
+		An int. the N th term in Fibonacci 
+		series of order k
+	'''
+
+	# add zeroes according to k
+	series = [0 for i in xrange(0,k-1)]
+
+	# add the 1 after all zeroes
+	series.append(1)
+
+	# obtain other terms by summ previous k terms
+	for i in xrange(0,N):
+		series.append(sum(series[-1:-(k+1):-1]))
+
+	return series[N]
