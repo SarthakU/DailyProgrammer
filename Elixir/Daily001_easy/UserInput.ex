4031900bbcc7be6319b2fb2be0c0c45b159d4da8
Sarthak Upadhyay <sarthak7u@gmail.com>
2022-12-12 17:36:28 +0530

    Add challenge #1 (easy) in Elixir

---
+++ Elixir/Daily001_easy/UserInput.ex
@@ -0,0 +1,13 @@
+## USER INPUT 
+##
+## challenge #1 (easy)
+## https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/ 
+## 
+## 
+## sarthak7u@gmail.com
+
+name = String.trim(IO.gets("Your Name: "))
+age = String.trim(IO.gets("Your Age: "))
+uname = String.trim(IO.gets("Your User Name: "))
+
+IO.puts("your name is #{name}, you are #{age} years old and your user name is #{uname}")
