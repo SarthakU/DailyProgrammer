33ef07635fbd5ecf2cb3f8dea1cd1165cc440129
Sarthak Upadhyay <sarthak7u@gmail.com>
2022-12-12 22:00:20 +0530

    Add challenge #1 (easy) in Bash

---
+++ Bash/Daily001_easy/UserInput.sh
@@ -0,0 +1,18 @@
+## User Input
+##
+## challenge #1 (easy)
+## https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/ 
+##
+##
+## sarthak7u@gmail.com
+
+echo "Your Name: "
+read -r name
+
+echo "Your Age: "
+read -r age
+
+echo "Your User Name: "
+read -r uname
+
+echo "your name is $name, you are $age yars old, and your username is $uname"
